df1e5e088593b61eee5c8f78a240979c9c565e4a
JulitaZochowska <julita.zochowska@outlook.com>
2023-05-20 23:00:36 +0200

    exercise 3 done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,16 @@
+//API
+// GET https://restcountries.com/v3.1/name/{name}
 const endpoint = 'https://restcountries.com/v3.1/name/';
+
+//https://restcountries.com/v3.1/{service}?fields={field},{field},{field}
+const filters = '?fields=name,capital,population,flags,languages';
 
 export const fetchCountries = name => {
   return new Promise((resolve, reject) => {
     //Tworzenie zadania HTTP do endpointa i przekazywanie
     //wyników jako tablicy krajów
 
-    fetch(endpoint + name)
+    fetch(endpoint + name + filters)
       .then(response => {
         if (!response.ok) {
           reject(`Error code ${response.status}`);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import { useDebounce } from 'use-lodash-debounce';
+import _ from 'lodash';
 
 const DEBOUNCE_DELAY = 300;
-const debounced = debounce(() => {
-  console.log('delayed');
-}, 300);
-
-//API
-// GET https://restcountries.com/v3.1/name/{name}
-const filterResponse =
-  'https://restcountries.com/v3.1/{service}?fields=name,capital,population,flags,languages';
 
 const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
 
 searchBox.addEventListener('input', handleSearch);
 
+const debounced = _.debounce(value => {
+  if (value === '') {
+    countryList.innerHTML = '';
+    return;
+  }
+  //debounced jest wrapem dla funkcji fetchCountries
+  //funkcja fetchCountries zwraca promisa dlatego dalej obsługujemy to jak jak promise (then)
+  //gdybym nie dała then(countries) to w konsoli nie wyświetliłaby się lista krajów
+  //to co zwrócone resolve (data) linia 18 FetchCountries.js wskakuje do then(countries)
+  fetchCountries(value).then(countries => {
+    //Zostawiam, aby sprawdzić w konsoli
+    console.log(countries);
+    //
+
+    let countriesListItems = '';
+    for (const country of countries) {
+      //country.name.official bo tak jest to opisane w API
+      countriesListItems += `<li>${country.name.official}</li>`;
+    }
+    countryList.innerHTML = countriesListItems;
+  });
+}, DEBOUNCE_DELAY);
+
+//bo debounced maił być na handlerze
 function handleSearch(event) {
-  fetchCountries(event.value);
+  debounced(event.target.value.trim());
 }
