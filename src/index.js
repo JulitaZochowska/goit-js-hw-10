0051df4dc4d4f5ef8ec45ac84d463d82ea6607fa
JulitaZochowska <julita.zochowska@outlook.com>
2023-05-23 22:27:38 +0200

    Finally version

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import _ from 'lodash';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -18,18 +19,45 @@ const debounced = _.debounce(value => {
   //funkcja fetchCountries zwraca promisa dlatego dalej obsługujemy to jak jak promise (then)
   //gdybym nie dała then(countries) to w konsoli nie wyświetliłaby się lista krajów
   //to co zwrócone resolve (data) linia 18 FetchCountries.js wskakuje do then(countries)
-  fetchCountries(value).then(countries => {
-    //Zostawiam, aby sprawdzić w konsoli
-    console.log(countries);
-    //
+  //czyli countries to odpowiedz serwera na nasz request
+  fetchCountries(value)
+    .then(countries => {
+      //Zostawiam, aby sprawdzić w konsoli
+      console.log(countries);
+      //
 
-    let countriesListItems = '';
-    for (const country of countries) {
-      //country.name.official bo tak jest to opisane w API
-      countriesListItems += `<li>${country.name.official}</li>`;
-    }
-    countryList.innerHTML = countriesListItems;
-  });
+      let countriesListItems = '';
+      if (countries.length >= 2 && countries.length <= 10) {
+        for (const country of countries) {
+          //country.name.official bo tak jest to opisane w API
+          countriesListItems += `<li> <img class="flag-img" src="${country.flags.svg}" alt="${country.name.officiale} flag" />${country.name.official}</li>`;
+        }
+        countryList.innerHTML = countriesListItems;
+        return;
+      }
+      if (countries.length === 1) {
+        const country = countries[0];
+        countryList.innerHTML = `<h2>${
+          country.name.official
+        }</h2> <p>Capital: ${country.capital}</p> <p>Population: ${
+          country.population
+        }</p> <p>Languages: ${Object.values(
+          country.languages
+        )}</p> <img class="flag-img" src="${country.flags.svg}" alt="${
+          country.name.officiale
+        } flag" />`;
+        return;
+      }
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+    })
+    .catch(err => {
+      Notiflix.Notify.failure('Oops, there is no country with that name.');
+    });
 }, DEBOUNCE_DELAY);
 
 //bo debounced maił być na handlerze
